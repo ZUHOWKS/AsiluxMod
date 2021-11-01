2a15cceb087e8a336884587d6e8523c6a9245bfe
ZUHOWKS <jojo.vilard38@gmail.com>
2021-11-01 11:43:46 +0100

    Apelle de la classe CommonProxy. Apelle de la classe RegistringEvent.

--- src/main/java/com/asiluxteam/zuhowks/AsiluxMod.java
+++ src/main/java/com/asiluxteam/zuhowks/AsiluxMod.java
@@ -1,7 +1,8 @@
 package com.asiluxteam.zuhowks;
 
+import com.asiluxteam.zuhowks.event.RegistringEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -23,20 +24,24 @@ public class AsiluxMod {
 
     private static Logger logger;
 
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
+    public AsiluxMod () {
+        MinecraftForge.EVENT_BUS.register(new RegistringEvent());
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
+        commonProxy.preInit();
+        logger = event.getModLog();
 
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        commonProxy.init();
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-
+        commonProxy.postInit();
     }
 }
