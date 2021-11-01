7cdd73e7f6ec5363a90c14c110927f4c8008c486
ZUHOWKS <jojo.vilard38@gmail.com>
2021-11-01 11:42:12 +0100

    Ajout de l'enregistrement des items.

--- src/main/java/com/asiluxteam/zuhowks/client/ClientProxy.java
+++ src/main/java/com/asiluxteam/zuhowks/client/ClientProxy.java
@@ -1,12 +1,19 @@
 package com.asiluxteam.zuhowks.client;
 
 import com.asiluxteam.zuhowks.CommonProxy;
+import com.asiluxteam.zuhowks.init.ItemsInit;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends CommonProxy {
 
+    private ItemsInit itemsInit = ItemsInit.INSTANCE;
+
     @Override
     public void preInit() {
+
         super.preInit();
+
+        MinecraftForge.EVENT_BUS.register(itemsInit);
     }
 
     @Override
